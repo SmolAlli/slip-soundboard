39f2c770b7d6f1e8c698b600a23c1a00f91c38f9
Alli <alli.toth@outlook.com>
2024-07-13 15:44:00 +1000

    Add initial files for the soundboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Box, Button, Grid, GridItem, Heading, Text, VStack } from '@chakra-ui/react';
+
+const sounds = {
+    'Point Redemptions': [
+        {
+            name: 'MA THE MEATLOAF',
+            file: 'ma-the-meatloaf.mp3',
+        },
+        {
+            name: 'BOO YOU STINK',
+            file: 'boo-you-stink.mp3',
+        },
+        {
+            name: 'shiet',
+            file: 'shiet.mp3',
+        },
+        {
+            name: 'do you think mr krabs ever does karate',
+            file: 'krabs-karate.mp3',
+        },
+    ],
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const playSound = (filename) => {
+        new Audio(`${process.env.PUBLIC_URL}/sounds/${filename}`).play();
+    };
+
+    return (
+        <Box className="App">
+            <Heading>Slip Soundboards</Heading>
+            <br />
+            <br />
+            <br />
+            <br />
+            <Grid templateColumns={'repeat(4, 1fr)'} gap={6}>
+                {Object.keys(sounds).map((soundCategory, i) => (
+                    <React.Fragment key={i}>
+                        <GridItem w="100%" h={20} colSpan={4}>
+                            <Heading size="md">{soundCategory}</Heading>
+                        </GridItem>
+                        {sounds[soundCategory].map((sound, j) => (
+                            <GridItem w="100%" h={20} key={j}>
+                                <VStack>
+                                    <Text>{sound.name}</Text>
+                                    <Button onClick={() => playSound(sound.file)}>Play {sound.name}</Button>
+                                </VStack>
+                            </GridItem>
+                        ))}{' '}
+                    </React.Fragment>
+                ))}
+            </Grid>
+        </Box>
+    );
 }
 
 export default App;
